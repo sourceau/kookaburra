a83340007bd0cb1e0cdb2a386c8411869f358479
sourceau <52984205+sourceau@users.noreply.github.com>
2024-07-21 21:48:19 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 //Test
 alert("working");
-import * as THREE from 'https://cdn.skypack.dev/three@0.150.0';
+import * as THREE from 'https://unpkg.com/three@0.142.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.150.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.150.0/examples/jsm/loaders/GLTFLoader.js';
 import { GLTFExporter } from 'https://cdn.jsdelivr.net/npm/three@0.150.0/examples/jsm/exporters/GLTFExporter.js';
